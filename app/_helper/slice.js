d1abd0c04d3da9446db6eeaf13d3e452f9591857
bradstrong <bradcstrong@gmail.com>
2013-10-01 11:46:16 -0400

    carousel v1

--- app/_helper/slice.js
+++ app/_helper/slice.js
@@ -4,16 +4,31 @@
 // usage: {{#slice items offset="3"}}{{name}}{{/slice}} : items 3 thru context.length
 // defaults are offset=0, limit=5
 // todo: combine parameters into single string like python or ruby slice ("start:length" or "start,length")
-Handlebars.registerHelper('slice', function(context, block) {
-  var ret = "",
-      offset = parseInt(block.hash.offset) || 0,
-      limit = parseInt(block.hash.limit) || 5,
-      i = (offset < context.length) ? offset : 0,
-      j = ((limit + offset) < context.length) ? (limit + offset) : context.length;
+// Handlebars.registerHelper('slice', function(context, block) {
+//   var ret = "",
+//       offset = parseInt(block.hash.offset) || 0,
+//       limit = parseInt(block.hash.limit) || 5,
+//       i = (offset < context.length) ? offset : 0,
+//       j = ((limit + offset) < context.length) ? (limit + offset) : context.length;
 
-  for(i,j; i<j; i++) {
-    ret += block(context[i]);
-  }
+//   for(i,j; i<j; i++) {
+//     ret += block(context[i]);
+//   }
 
-  return ret;
-});
+//   return ret;
+// });
+module.exports.register = function (Handlebars, options) {
+  Handlebars.registerHelper('slice', function(context, block) {
+    var ret = "",
+        offset = parseInt(block.hash.offset) || 0,
+        limit = parseInt(block.hash.limit) || 5,
+        i = (offset < context.length) ? offset : 0,
+        j = ((limit + offset) < context.length) ? (limit + offset) : context.length;
+
+    for(i,j; i<j; i++) {
+      ret += block(context[i]);
+    }
+
+    return ret;
+  });
+};
